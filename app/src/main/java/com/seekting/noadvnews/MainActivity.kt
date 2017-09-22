c2176b53d54f909c8533b30a6bce0733cd0defd8
zhangxingting <zhangxingting@mobimagic.com>
2017-09-22 21:27:56 +0800

    rename

--- app/src/main/java/com/seekting/noadvnews/MainActivity.kt
+++ app/src/main/java/com/seekting/noadvnews/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.seekting.kotlindemo
+package com.seekting.noadvnews
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -6,10 +6,13 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("seekting","hello")
+        Log.d("seekting", "hello")
+        var appid = "46647"
+        var appkey = "f8226efcfa85460a8f3d703eb08a1689"
     }
 
     override fun onPause() {
