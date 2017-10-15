32bf42b41de06fdc688bc19e7126a5d4373a4bf8
seekting <543090717@qq.com>
2017-10-15 22:16:45 +0800

    lib

---
+++ external/recyclerview-swipe/src/main/java/com/yanzhenjie/MyViewCompat.java
@@ -0,0 +1,14 @@
+package com.yanzhenjie;
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/10/15.
+ */
+
+public class MyViewCompat {
+    public static void setBackground(View parent, Drawable background) {
+        parent.setBackground(background);
+    }
+}
